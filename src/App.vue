42601645f50fa24b6e3a8d24a907a135708b6e4a
Jake Espinosa <jacob.sa.espinosa@gmail.com>
2024-03-02 22:25:41 -0700

    Make responsive

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,9 @@
 <template>
   <div class="wrapper">
-    <nav>nav</nav>
+    <nav class="mobile">mobile-nav</nav>
+    <nav class="desktop">desktop-nav</nav>
     <main>main</main>
-    <footer>footer</footer>
+    <footer class="mobile">mobile-footer</footer>
+    <footer class="desktop">desktop-footer</footer>
 </div>
 </template>
