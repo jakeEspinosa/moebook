223310d6d490a311ee1bb5341e9f6ac493c65a81
Jake Espinosa <jacob.sa.espinosa@gmail.com>
2024-03-02 23:30:41 -0700

    Add mobile header and style

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,10 @@
+<script setup lang="ts">
+  import MobileHeader from "@/components/header/MobileHeader.vue";
+</script>
+
 <template>
   <div class="wrapper">
-    <nav class="mobile">mobile-nav</nav>
+    <MobileHeader class="mobile" />
     <nav class="desktop">desktop-nav</nav>
     <main>main</main>
     <footer class="mobile">mobile-footer</footer>
---
+++ src/components/header/MobileHeader.vue
@@ -0,0 +1,11 @@
+<template>
+  <nav>
+    <h1>moebook</h1>
+  </nav>
+</template>
+
+<style scoped>
+  h1 {
+    color: var(--main-header-color);
+  }
+</style>
