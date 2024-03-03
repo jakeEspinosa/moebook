942c1550bc31b645cd765c25879b6f699cb14644
Jake Espinosa <jacob.sa.espinosa@gmail.com>
2024-03-03 00:19:38 -0700

    Style

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,12 @@
 <script setup lang="ts">
   import MobileHeader from "@/components/header/MobileHeader.vue";
+  import DesktopHeader from "@/components/header/DesktopHeader.vue";
 </script>
 
 <template>
   <div class="wrapper">
     <MobileHeader class="mobile" />
-    <nav class="desktop">desktop-nav</nav>
+    <DesktopHeader class="desktop" />
     <main>main</main>
     <footer class="mobile">mobile-footer</footer>
     <footer class="desktop">desktop-footer</footer>
---
+++ src/components/header/DesktopHeader.vue
@@ -0,0 +1,33 @@
+<template>
+  <nav>
+    <div class="flex-container">
+      <h1>moebook</h1>
+      <div class="flex-container space-between">
+        <ul class="flex-container">
+          <li>Home</li>
+          <li>Friends</li>
+          <li>Posts</li>
+        </ul>
+        <p>Profile Icon</p>
+      </div>
+      <hr />
+    </div>
+  </nav>
+</template>
+  
+<style scoped>
+  h1 {
+    color: var(--main-header-color);
+    padding-left: 1em;
+  }
+  .flex-container {
+    display: flex;
+    align-items: center;
+    gap: 1rem;
+  }
+  .space-between {
+    justify-content: space-between;
+    flex: 1;
+  }
+</style>
+  
